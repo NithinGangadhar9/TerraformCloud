083cd3887a8b8dddbf4078bcb7333791912d9dea
NithinGangadhar9 <>
2021-05-13 18:32:52 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
+sudo amazon-linux-extras install nginx1 -y # Deploy nginx webserver on Amazon Linux
 sudo systemctl start nginx
 sudo systemctl status nginx
