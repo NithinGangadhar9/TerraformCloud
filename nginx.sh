66d70f947e343719cfd5e33a6f79809084d39072
NithinGangadhar9 <>
2021-05-13 17:16:20 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install nginx
-
-# List the application configurations that ufw 
-sudo ufw app list
-sudo ufw allow 'Nginx HTTP'
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl start nginx
+sudo systemctl status nginx
